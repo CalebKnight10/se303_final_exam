6717da99ee7ca8e88d1bed1cc02a21f1dc2b4850
Caleb Knight <knightca@oregonstate.edu>
2022-06-07 10:45:13 -0700

    Added midPart function that holds the middle part of the message

--- apple/lib/main.rb
+++ apple/lib/main.rb
@@ -6,14 +6,18 @@ def firstPart
   "Mr. Dursley was the director of a firm called Grunnings, which made drills."
 end
 
-def story
-  first = firstPart
+def midPart
   middle = ""
   middle << "\n"
   middle << "\n"
   middle << "\n"
   middle << "\n"
   middle << "\n"
+end
+
+def story
+  first = firstPart
+  middle = midPart
   conclusion = "And that was it."
   first + middle + conclusion
 end
